7466ae820113f199a27f973542ab0fd466e63c9e
Juan <gurquiza8@gmail.com>
2024-01-21 13:45:11 -0600

    Creating validators for request with express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,29 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarUsuario } = require('../controllers/auth');
 
 const router = Router();
 
-router.post('/new', crearUsuario);
-
-router.post('/', loginUsuario);
+router.post(
+    '/new',
+    [   // Middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+    ],
+    crearUsuario
+);
+        
+router.post(
+    '/',
+    [   // Middlewares
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+    ],
+    loginUsuario
+);
 
 router.get('/renew', revalidarUsuario);
 
