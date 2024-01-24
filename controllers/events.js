a2f102231aa8f37b602a9a1dd7ff6a8774e295c6
Juan <gurquiza8@gmail.com>
2024-01-23 18:31:39 -0600

    Agregando eventos a la bbdd y modificando json de respuesta

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Evento = require('../models/Evento');
 
 const getEventos = (req, res = response) => {
 
@@ -9,11 +10,30 @@ const getEventos = (req, res = response) => {
     
 }
 
-const crearEvento = (req, res = response) => {
+const crearEvento = async(req, res = response) => {
 
-    // Verificar que tanga el evento
-    console.log(req.body);
-    
+    const evento = new Evento( req.body );
+
+    try {
+
+        // Obtener el id del usuario.
+        evento.user = req.uid;
+
+        const eventoGuardado = await evento.save();
+
+        res.status(201).json({
+            ok: true,
+            evento: eventoGuardado
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
     return res.status(200).json({
         ok: true,
         msg: 'crearEventos',
