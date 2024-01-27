227203e0d62d754603b198fe1414f5a545ee1dc3
Juan <gurquiza8@gmail.com>
2024-01-27 13:58:54 -0600

    Adding endpoint edit event

--- controllers/events.js
+++ controllers/events.js
@@ -43,12 +43,48 @@ const crearEvento = async(req, res = response) => {
 
 }
 
-const actualizarEvento = (req, res = response) => {
+const actualizarEvento = async(req, res = response) => {
 
-    return res.status(200).json({
-        ok: true,
-        msg: 'actualizarEvento',
-    });
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await Evento.findById( eventoId );
+
+        if ( !evento ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Ese id no existe'
+            });
+        }
+
+        if ( evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegios de editar este evento'
+            });
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true } );
+
+        return res.status(201).json({
+            ok: true,
+            evento: eventoActualizado
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return RegExp.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador.'
+        })
+    }
 
 }
 
